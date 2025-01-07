b53cd8d79ff7e696b9e44a89e4935e99b52707c9
Giulia Gallo <giulia.gallo2903@gmail.com>
2025-01-07 16:58:24 +0100

    main3

---
+++ Main3.java
@@ -0,0 +1,8 @@
+public class Main3 {
+    public static void main(String[] args) {
+        double p = sum(2.5,4.5);
+        System.out.println(p);
+
+    }
+    public static double sum(double a, double b) {return a*2 + b*2;}
+}
