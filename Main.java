294423a6f05e2e5d7cf00ca895e4be64c7e6b46a
Giulia Gallo <giulia.gallo2903@gmail.com>
2025-01-07 16:23:27 +0100

    start

---
+++ Main.java
@@ -0,0 +1,24 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Ciao, Java!");
+
+        int num1 = 7;
+        int sum = num1 * 5;
+        System.out.println(sum);
+
+        String firstWord = "Ciao";
+        System.out.println(sum + " " + firstWord);
+
+        String[] a = {"A", "B", "C", "D", "E"};
+        String newString= "F";
+        
+
+
+    }
+
+        
+    }
+
